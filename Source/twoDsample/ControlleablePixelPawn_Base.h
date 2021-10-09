58d23049d2e2f790c3d48cf04e817a66468e011b
Yorda <921864047@qq.com>
2021-10-09 23:05:19 +0900

    first update

---
+++ Source/twoDsample/ControlleablePixelPawn_Base.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Pixel2DCharacter.h"
+#include "ControlleablePixelPawn_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TWODSAMPLE_API AControlleablePixelPawn_Base : public APixel2DCharacter
+{
+	GENERATED_BODY()
+	
+};
